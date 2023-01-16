f4c7c11d8831610c37939728ac1a692bd57daf79
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-01-16 02:18:30 +0100

    update algo

---
+++ components/content/algotri.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div>
+      <label>Vitesse d'animation (ms):</label>
+      <input type="number" v-model="animationSpeed" />
+    </div>
+    <button @click="runInsertionSort">Tri par insertion</button>
+    <transition-group name="slide" tag="div" style="height: 30px; position: relative; margin-top: 2rem;">
+      <div v-for="(item, index) in numbers" :key="index" :style="{ height: 3*item.val + 'px', order: item.pos, left: item.pos*35 + 'px' }" class="number-bar" :class="{ 'active': activeIndex === index }" />
+    </transition-group>
+  </div>
+</template>
+
+<script setup>
+const numbers = ref([5, 3, 8, 10, 2, 9, 1, 7, 12])
+const animationSpeed = ref(1000)
+const activeIndex = ref(null)
+
+for (let i = 0; i < numbers.value.length; i++) {
+  numbers.value[i] = {pos: i, val: numbers.value[i]}
+}
+
+const moveItem = (pos, val) => {
+  //numbers.value.push({ pos: 3, val: 7})
+  //numbers.value[2].pos += 1
+  const firstindex = numbers.value.findIndex(e => e.pos === pos)
+  const othindex = numbers.value.findIndex(e => e.pos === pos+val)
+  numbers.value[firstindex].pos += val
+  numbers.value[othindex].pos -= val
+
+}
+const runInsertionSort = async () => {
+  for (let i = 1; i < numbers.value.length; i++) {
+    activeIndex.value = numbers.value.findIndex(e => e.pos === i)
+    await sleep(animationSpeed.value)
+    let current = numbers.value[activeIndex.value].val
+    let j = i - 1
+    while (j >= 0 && numbers.value.find(e => e.pos === j).val > current) {
+      moveItem(j, +1)
+      j--
+      await sleep(animationSpeed.value)
+    }
+  }
+  activeIndex.value = null
+}
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+</script>
+
+<style scoped>
+.number-bar {
+  position: absolute;
+  bottom: 0;
+  width: 30px;
+  margin-right: 5px;
+  background-color: aqua;
+  border-radius: 5px;
+  transition: left 0.5s ease-in 0s;
+}
+
+.active {
+  background-color: green;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 1s ease;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(30px);
+  opacity: 0;
+}
+</style>
