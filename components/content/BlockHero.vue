6455d5f2560d3f8b9345aa001bb4cb9ccfc365f0
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-05-02 01:29:40 +0200

    correction blockhero

--- components/content/BlockHero.vue
+++ components/content/BlockHero.vue
@@ -4,19 +4,23 @@ import type { PropType } from 'vue'
 defineProps({
   cta: {
     type: Array as PropType<string[]>,
-    required: false
+    required: false,
+    default: () => []
   },
   secondary: {
     type: Array as PropType<string[]>,
-    required: false
-  },
-  snippet: {
-    type: String,
-    required: false
+    required: false,
+    default: () => []
   },
   video: {
-    type: Array as PropType<string[]>,
-    required: false
+    type: String,
+    required: false,
+    default: ''
+  },
+  snippet: {
+    type: [Array, String] as PropType<string[] | string>,
+    required: false,
+    default: ''
   }
 })
 </script>
@@ -29,12 +33,16 @@ defineProps({
           <ContentSlot :use="$slots.announce" unwrap="p" />
         </p>
 
-        <h1 v-if="$slots.title" class="title">
-          <ContentSlot :use="$slots.title" unwrap="p" />
+        <h1 class="title">
+          <ContentSlot :use="$slots.title" unwrap="p">
+            Hero Title
+          </ContentSlot>
         </h1>
 
-        <p v-if="$slots.description" class="description">
-          <ContentSlot :use="$slots.description" unwrap="p" />
+        <p class="description">
+          <ContentSlot :use="$slots.description" unwrap="p">
+            Hero default description.
+          </ContentSlot>
         </p>
 
         <div v-if="$slots.extra" class="extra">
@@ -42,16 +50,15 @@ defineProps({
         </div>
 
         <div class="actions">
-          <template v-if="!$slots.actions">
+          <ContentSlot v-if="$slots.actions" :use="$slots.actions" unwrap="p" />
+          <template v-else>
             <ButtonLink v-if="cta" class="cta" bold size="medium" :href="(cta[1] as any)">
               {{ cta[0] }}
             </ButtonLink>
-
             <a v-if="secondary" :href="(secondary[1] as any)" class="secondary">
               {{ secondary[0] }}
             </a>
           </template>
-          <ContentSlot v-else :use="$slots.actions" unwrap="p" />
         </div>
       </div>
 
@@ -65,89 +72,97 @@ defineProps({
 
 <style scoped lang="ts">
 css({
-'.block-hero': {
-padding: '{space.20} 0',
-'@sm': {
-       padding: '{space.24} 0',
-     },
-'@lg': {
-       padding: '{space.32} 0',
-     },
-'.layout': {
-display: 'grid',
-gap: '{space.8}',
-'@lg': {
-       gridTemplateColumns: 'repeat(3, minmax(0, 1fr))'
-     }
-},
-'.content': {
-'@lg': {
-       gridColumn: 'span 2 / span 2'
-     },
-'.announce': {
-marginBottom: '{space.2}',
-textAlign: 'center',
-'@lg': {
-       textAlign: 'left'
-     }
-},
-'.title': {
-color: '{elements.color.text.primary.default}',
-fontWeight: '{fontWeight.bold}',
-letterSpacing: '{letterSpacing.tight}',
-textAlign: 'center',
-fontSize: '{text.4xl.fontSize}',
-lineHeight: '{text.4xl.lineHeight}',
-'@sm': {
-       fontSize: '{text.5xl.fontSize}',
-       lineHeight: '{text.5xl.lineHeight}',
-     },
-'@lg': {
-       fontSize: '{text.6xl.fontSize}',
-       lineHeight: '{text.6xl.lineHeight}',
-       textAlign: 'left'
-     }
-},
-'.description': {
-marginTop: '{space.4}',
-fontSize: '{text.lg.fontSize}',
-lineHeight: '{text.lg.lineHeight}',
-textAlign: 'center',
-color: '{elements.color.text.secondary.default}',
-'@lg': {
-       textAlign: 'left'
-     }
-},
-'.extra': {
-          marginTop: '{space.6}'
-        },
-'.actions': {
-display: 'flex',
-flexDirection: 'column',
-alignItems: 'center',
-justifyContent: 'center',
-gap: '{space.4}',
-marginTop: '{space.6}',
-'@sm': {
-       marginTop: '{space.10}',
-       flexDirection: 'row',
-       gap: '{space.6}'
-     },
-'@lg': {
-       justifyContent: 'flex-start'
-     },
-'.cta': {
-        marginBottom: 0
+  '.block-hero': {
+    padding: '{space.20} 0',
+    '@sm': {
+      padding: '{space.24} 0',
+    },
+    '@lg': {
+      padding: '{space.32} 0',
+    },
+    '.layout': {
+      display: 'grid',
+      gap: '{space.16}',
+      '@lg': {
+        gap: '{space.8}',
+        gridTemplateColumns: 'repeat(3, minmax(0, 1fr))'
+      }
+    },
+    '.content': {
+      '@lg': {
+        gridColumn: 'span 2 / span 2'
       },
-'.secondary': {
-fontWeight: '{fontWeight.medium}',
-color: '{elements.color.text.secondary.default}',
-'&:hover': {
-           color: '{elements.color.text.secondary.hover}'
-         }
-}
-}
-}
-}
+      '.announce': {
+        marginBottom: '{space.2}',
+        textAlign: 'center',
+        '@lg': {
+          textAlign: 'start'
+        }
+      },
+      '.title': {
+        color: '{elements.text.primary.color.static}',
+        fontWeight: '{fontWeight.bold}',
+        letterSpacing: '{letterSpacing.tight}',
+        textAlign: 'center',
+        fontSize: '{text.4xl.fontSize}',
+        lineHeight: '{text.4xl.lineHeight}',
+        marginBottom: '{space.8}',
+        px: '{space.8}',
+        '@sm': {
+          fontSize: '{text.5xl.fontSize}',
+          lineHeight: '{text.5xl.lineHeight}',
+        },
+        '@lg': {
+          px: '{space.0}',
+          fontSize: '{text.6xl.fontSize}',
+          lineHeight: '{text.6xl.lineHeight}',
+          textAlign: 'start'
+        }
+      },
+      '.description': {
+        marginBottom: '{space.12}',
+        fontSize: '{text.lg.fontSize}',
+        lineHeight: '{text.lg.lineHeight}',
+        textAlign: 'center',
+        color: '{elements.text.secondary.color.static}',
+        px: '{space.2}',
+        '@lg': {
+          textAlign: 'start'
+        }
+      },
+      '.extra': {
+        marginBottom: '{space.16}',
+        display: 'flex',
+        justifyContent: 'center',
+        '@lg': {
+          justifyContent: 'flex-start'
+        }
+      },
+      '.actions': {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '{space.4}',
+        '@sm': {
+          flexDirection: 'row',
+          gap: '{space.6}'
+        },
+        '@lg': {
+          justifyContent: 'flex-start'
+        },
+        '.cta': {
+          marginBottom: 0
+        },
+        '.secondary': {
+          fontWeight: '{fontWeight.medium}',
+          color: '{elements.text.secondary.color.static}',
+          '&:hover': {
+            color: '{elements.text.secondary.color.hover}'
+          }
+        }
+      }
+    }
+  }
 })
 </style>
