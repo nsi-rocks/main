09ec3cdca64616fbb5751de6bd4b943bc0682c0c
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-05-02 02:21:30 +0200

    ok

--- components/content/Logo.vue
+++ components/content/Logo.vue
@@ -1,3 +1,4 @@
 <template>
-  <image src="/img/logo.png" iwidth="200" />
+  <img src="/img/logo-d.png" v-if="$colorMode.value == 'dark'" width="200" />
+  <img src="/img/logo.png" v-if="$colorMode.value == 'light'" width="200" />
 </template>
