135dc033800aef06797f36c8c8e6a17a654ba5c0
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-11-05 23:36:51 +0100

    new component

---
+++ components/content/couleurs-rgb.vue
@@ -0,0 +1,31 @@
+<template>
+  <UCard :ui="{ background: 'bg-gray-50 dark:bg-gray-800 ring-gray-200 dark:ring-gray-700'}">
+  <template #header>
+    <div class="flex flex-row justify-center font-semibold text-lg">
+    RGB : ({{ rouge }}, {{ vert }}, {{ bleu }})
+    </div>
+  </template>
+
+  <template #footer>
+    <UCard :style="`background-color: rgb(${rouge}, ${vert}, ${bleu})`" />
+  </template>
+
+  <div class="flex flex-col gap-4">
+    <URange size="2xl" color="red" :min="0" :max="255" v-model="rouge" />
+    <URange size="2xl" color="green" :min="0" :max="255" v-model="vert" />
+    <URange size="2xl" color="blue" :min="0" :max="255" v-model="bleu" />
+  </div>
+  </UCard>
+
+</template>
+
+<script lang="ts" setup>
+const rouge = ref(0)
+const vert = ref(0)
+const bleu = ref(0)
+</script>
+
+<style>
+
+</style>
+
