a7f1d247f2f431346ba72d1190fa5a48b362e1e6
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-11-05 23:36:57 +0100

    old components updates

---
+++ components/content/PCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: ''
+  },
+  iconClass: {
+    type: String,
+    default: ''
+  },
+  blurry: {
+    type: Boolean,
+    default: true,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <div class="ring-1 rounded-lg p-2">
+    <Icon v-if="icon" :name="icon" />
+    <slot />
+    <div>
+      <h3 class="m-0" v-if="$slots.title">
+        <ContentSlot :use="$slots.title" unwrap="p">
+          Card title
+        </ContentSlot>
+      </h3>
+      <div class="description" v-if="$slots.description">
+        <ContentSlot :use="$slots.description">
+          Card description
+        </ContentSlot>
+      </div>
+    </div>
+  </div>
+</template>
