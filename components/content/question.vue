f413b6a08cf5f0ac2c896091d9f90b8cdc28f68c
Mathieu NICOLAS <mathieu.nicolas@me.com>
2023-11-05 14:08:13 +0100

    quizz module

---
+++ components/content/question.vue
@@ -0,0 +1,33 @@
+<template>
+  <UCard>
+    <template #header>
+      <ContentSlot :use="$slots.question" />
+    </template>
+    <template #footer v-if="expli !== -1">
+      <ContentSlot :name="expli" />
+    </template>
+  <div class="flex flex-row justify-around gap-2">
+    <UButton v-for="(r, index) of reps.sort(() => Math.random() - 0.5)" :label="r.label" @click="verif(r)" variant="soft" />
+  </div>
+  </UCard>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(['reponses'])
+const emits = defineEmits(['check'])
+const reps = props.reponses.split(',').map((el, index) => {
+  const tmp = el.trim().split('.')
+  return { label: tmp[1], rep: tmp[0], idx: index}
+})
+const expli = ref(-1)
+
+const verif = (r) => {
+  r.idx === 0 ? emits('check', true) : emits('check', false)
+  expli.value = r.rep
+}
+</script>
+
+<style>
+
+</style>
+
