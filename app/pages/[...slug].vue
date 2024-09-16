1b27849658ef30e29c071a7eb2ef16ce4939f36e
Mathieu NICOLAS <mathieu@Mathieus-MacBook-Pro.local>
2024-09-17 00:26:41 +0200

    fix

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -2,15 +2,17 @@
 </script>
 
 <template>
-  <ContentDoc :head="false">
-    <template #not-found>
-      Oops... La page que vous cherchez n'existe pas.
-    </template>
-    <template #default="{ doc }">
-      <article class="xl:w-2/3 mx-auto mb-8">
-        <h1>{{ doc.title }}</h1>
-        <ContentRenderer :value="doc" />
-      </article>
-    </template>
-  </ContentDoc>
+  <div>
+    <ContentDoc :head="false">
+      <template #not-found>
+        Oops... La page que vous cherchez n'existe pas.
+      </template>
+      <template #default="{ doc }">
+        <article class="xl:w-2/3 mx-auto mb-8">
+          <h1>{{ doc.title }}</h1>
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+    </ContentDoc>
+  </div>
 </template>
