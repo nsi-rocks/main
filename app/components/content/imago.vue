b8108baf6a5e6f7b5ff55f2c0aa750b6c05333eb
Mathieu NICOLAS <mathieu@Mathieus-MacBook-Pro.local>
2024-09-17 00:26:38 +0200

    composant pour insérer une image qui prend en compte le thème sombre/clair

---
+++ app/components/content/imago.vue
@@ -0,0 +1,23 @@
+<template>
+  <ClientOnly>
+    <figure>
+      <img :src="`/${ld_src}`" style="margin: auto;">
+      <figcaption style="text-align: center;">
+        <slot />
+      </figcaption>
+    </figure>
+  </ClientOnly>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  src: String,
+})
+
+const c = useColorMode()
+const ld_src = computed(() => c.value === 'dark' ? props.src!.replace('.png', '-d.png') : props.src)
+</script>
+
+<style>
+
+</style>
