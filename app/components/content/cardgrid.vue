58c26fb60bd290c7a66b98e1f2b314f177e04109
Mathieu NICOLAS <mathieu.nicolas@me.com>
2024-10-01 20:07:29 +0200

    style

--- app/components/content/cardgrid.vue
+++ app/components/content/cardgrid.vue
@@ -1,23 +1,12 @@
 <template>
-  <div :class="ui.wrapper+' grid-cols-'+cols" v-bind="attrs">
+  <div class="grid gap-4" :class="`grid-cols-${cols}`">
     <slot />
   </div>
 </template>
 
 <script setup lang="ts">
-defineOptions({
-  inheritAttrs: false,
-})
-
 const props = defineProps<{
-  ui?: Partial<typeof config>
   cols: number
-  class?: any
+  cclass?: any
 }>()
-
-const config = {
-  wrapper: `grid gap-8`,
-}
-
-const { ui, attrs } = useUI('page.grid', toRef(props, 'ui'), config, toRef(props, 'class'), true)
 </script>
--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -8,7 +8,7 @@
     </template>
     <template #default="{ doc }">
       <UPage>
-        <UPageBody prose>
+        <UPageBody prose class="prose-code:text-base">
           <article class="xl:w-2/3 mx-auto mb-8">
             <h1>{{ doc.title }}</h1>
             <ContentRenderer :value="doc" />
