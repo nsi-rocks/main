f0ca1f57647a2af25d67a88ac8ecb053bb140e24
Mathieu NICOLAS <mathieu@Mathieus-MacBook-Pro.local>
2024-10-03 01:31:15 +0200

    ajout module rgb [test]

---
+++ app/pages/rgb.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="flex flex-row items-start my-12 justify-between">
+    <div class="grow self-center">
+      <div class="grid gap-2 m-auto w-fit" :style="grcols">
+        <div
+          v-for="i in cases"
+          :key="i"
+          class="border-2 box-border cursor-pointer hover:border-4"
+          :class="(curr === i-1 ? 'ring-4 ring-blue-500' : '') "
+          :style="getbg(i-1) + `width: ${w}rem;`"
+          style="aspect-ratio: 1 / 1;"
+          draggable="true"
+          @click="handleClick(i-1)"
+          @dragstart="handleDragStart($event, data[i-1])"
+          @dragenter="enterDrag"
+          @dragleave="leaveDrag"
+          @dragover.prevent
+          @drop="drop($event, i-1)"
+        >
+&nbsp;
+        </div>
+      </div>
+    </div>
+    <div class="w-1/3 flex flex-col gap-2 p-4 border">
+      <URange
+        v-model="w"
+        :step="0.5"
+        :min="1"
+        :max="8"
+      />
+      <UTabs v-model="mode" :items="items" class="mb-12">
+        <template #item="{ item }">
+          <template v-if="item.key === 'rgb'">
+            <div class="font-semibold text-xl flex justify-center">
+              <span>r: {{ data[curr].r }}, g: {{ data[curr].g }}, b: {{ data[curr].b }}</span>
+            </div>
+            <div v-for="a in sliders" :key="a">
+              <UFormGroup :label="a.label">
+                <URange
+                  v-model="data[curr][a.key]"
+                  :color="a.color"
+                  :step="1"
+                  :min="0"
+                  :max="255"
+                />
+              </UFormGroup>
+            </div>
+          </template>
+        </template>
+      </UTabs>
+      <UButton v-if="mode === 1" class="self-center" @click="resetCases(data[curr])">
+        Appliquer à toutes
+      </UButton>
+      <UButton class="self-center" @click="resetCases">
+        Remise à zéro
+      </UButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const ca = parseInt(useRoute().query.cases as string || '3')
+const cases = ca * ca
+const grcols = `grid-template-columns: repeat(${ca}, minmax(0, 1fr));`
+const w = ref(2)
+const base = { r: 0, g: 0, b: 0 }
+const curr = ref(1)
+const mode = ref(0)
+
+const items = [{
+  key: 'bw',
+  label: 'Noir et blanc',
+}, {
+  key: 'rgb',
+  label: 'Couleurs',
+}]
+const sliders = [{
+  label: 'Rouge',
+  color: 'red',
+  key: 'r',
+}, {
+  label: 'Vert',
+  color: 'green',
+  key: 'g',
+}, {
+  label: 'Bleu',
+  color: 'blue',
+  key: 'b',
+}]
+const data = ref(Array.from({ length: cases }, () => ({ ...base })))
+
+const getbg = (i: number) => {
+  const color = data.value[i] || { r: 0, g: 0, b: 0 }
+  const { r, g, b } = color
+  return `background-color: rgb(${r},${g},${b});`
+}
+
+const handleDragStart = (e: DragEvent, itemData) => {
+  e.dataTransfer.setData('text/plain', JSON.stringify(itemData))
+}
+
+const enterDrag = (e) => {
+  e.preventDefault()
+  e.target.classList.add('dragover')
+}
+
+const leaveDrag = (e) => {
+  e.preventDefault()
+  e.target.classList.remove('dragover')
+}
+
+const drop = (e, i) => {
+  e.preventDefault()
+  e.target.classList.remove('dragover')
+  data.value[i] = JSON.parse(e.dataTransfer.getData('text/plain'))
+}
+
+const handleClick = (i: number) => {
+  if (mode.value === 1)
+    curr.value = i
+  else {
+    const sum = data.value[i].r + data.value[i].g + data.value[i].b
+    if (sum < 382)
+      data.value[i] = { r: 255, g: 255, b: 255 }
+    else
+      data.value[i] = { r: 0, g: 0, b: 0 }
+  }
+}
+
+const resetCases = (color?) => {
+  data.value = Array.from({ length: cases }, () => ({ ...base, ...color }))
+}
+</script>
+
+<style>
+.dragover {
+  border: 4px dashed gray;
+}
+</style>
