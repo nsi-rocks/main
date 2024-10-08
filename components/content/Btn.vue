4307ed29dadb1a9d869698d2af7feef3ae2cef55
Mathieu NICOLAS <mathieu@Mathieus-MacBook-Pro.local>
2024-10-09 00:19:14 +0200

    delete everything else

--- components/content/Btn.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <nuxt-link :to="{ path: './algo-tri', hash: '#tri-par-insertion'}">
-  test
-  </nuxt-link>
-</template>
--- components/content/Logo.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <img src="/img/logo-d.png" v-if="$colorMode.value == 'dark'" width="200" />
-  <img src="/img/logo.png" v-if="$colorMode.value == 'light'" width="200" />
-</template>
