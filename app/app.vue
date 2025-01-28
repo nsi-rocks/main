d8c249398d0c4b093604ebf205093bea8c6d2ae7
Mathieu NICOLAS <mathieu@Mathieus-MacBook-Pro.local>
2025-01-28 12:53:58 +0100

    add banner + container 100%

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
 <template>
   <UApp>
+    <UBanner
+      v-if="isFirefox"
+      title="Ce site n'est pour l'instant pas complètement optimisé pour le navigateur Firefox, veuillez utiliser Google Chrome."
+      close
+    />
     <NuxtLayout :appid="appid">
       <NuxtPage />
     </NuxtLayout>
@@ -23,5 +28,13 @@ useHead({
     { name: 'description', content: 'NSI Rocks !' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
   ],
+})
+
+const isFirefox = ref(false)
+
+onMounted(() => {
+  // Vérifier si l'utilisateur utilise Firefox
+  const userAgent = navigator.userAgent.toLowerCase()
+  isFirefox.value = userAgent.includes('firefox')
 })
 </script>
