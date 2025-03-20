7198f1fd1e93844085c702548a4659ba9ef22fea
Mathieu NICOLAS <mathieu@Mathieus-MacBook-Pro.local>
2025-03-21 00:03:08 +0100

    update links

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -23,39 +23,36 @@ if (await denies(userOrDev)) {
 }
 
 const route = useRoute()
-const links = computed(() => {
-  const data = [
-    {
-      label: 'Dashboard',
-      to: '/dashboard',
-      active: route.path === '/dashboard',
-    },
-    {
-      label: 'RGB',
-      to: '/dashboard/rgb',
-      active: route.path.startsWith('/dashboard/rgb'),
-    },
-    {
-      label: 'API',
-      to: '/dashboard/api',
-      active: route.path.startsWith('/dashboard/api'),
-    }
-  ]
-
-  if (import.meta.dev) {
-    data.push({
-      label: 'Eleves',
-      to: '/dashboard/eleves',
-      active: route.path.startsWith('/dashboard/eleves'),
-    })
-    data.push({
-      label: 'Logs',
-      to: '/dashboard/logs',
-      active: route.path.startsWith('/dashboard/logs'),
-    })
+const links = [
+  {
+    label: 'Dashboard',
+    to: '/dashboard',
+    active: route.path === '/dashboard',
+  },
+  {
+    label: 'RGB',
+    to: '/dashboard/rgb',
+    active: route.path.startsWith('/dashboard/rgb'),
+  },
+  {
+    label: 'API',
+    to: '/dashboard/api',
+    active: route.path.startsWith('/dashboard/api'),
   }
-  return data
-})
+]
+
+if (await allows(adminOrDev)) {
+  links.push({
+    label: 'Eleves',
+    to: '/dashboard/eleves',
+    active: route.path.startsWith('/dashboard/eleves'),
+  })
+  links.push({
+    label: 'Logs',
+    to: '/dashboard/logs',
+    active: route.path.startsWith('/dashboard/logs'),
+  })
+}
 </script>
 
 <style scoped></style>
